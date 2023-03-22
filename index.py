80d5e84a844212561ac0588444bb706e51d788e3
Muhu-Muza <schumannivan@gmail.com>
2023-03-22 23:08:48 +0300

    Revert "added mime type"
    This reverts commit f72ad54d3decb460a70f155d2a266458c28b2daa.

--- index.py
+++ index.py
@@ -7,23 +7,23 @@ def create_app():
 
     @app.route('/index')
     def index():
-        return render_template("index.html"), {'Content-Type': 'text/html'}
+        return render_template("index.html")
 
     @app.route('/about')
     def about():
-        return render_template("about.html"), {'Content-Type': 'text/html'}
+        return render_template("about.html")
 
     @app.route('/blog')
     def blog():
-        return render_template("blog.html"), {'Content-Type': 'text/html'}
+        return render_template("blog.html")
 
     @app.route('/contact')
     def contact():
-        return render_template("contact.html"), {'Content-Type': 'text/html'}
+        return render_template("contact.html")
 
     @app.route('/services')
     def services():
-        return render_template("services.html"), {'Content-Type': 'text/html'}
+        return render_template("services.html")
 
     return app
 
